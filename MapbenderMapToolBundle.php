d50a68603f26d97a9a2edece4cac09758f6bdf75
Paul Schmidt <paul.schmidt@wheregroup.com>
2016-03-04 14:23:45 +0100

    Add bundle class file

---
+++ MapbenderMapToolBundle.php
@@ -0,0 +1,23 @@
+<?php
+namespace Mapbender\MapToolBundle;
+
+use Mapbender\CoreBundle\Component\MapbenderBundle;
+
+/**
+ * Mapbender map tool bundle
+ *
+ */
+class MapbenderMapToolBundle extends MapbenderBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public function getElements()
+    {
+        return array(
+            
+        );
+    }
+
+}
+
